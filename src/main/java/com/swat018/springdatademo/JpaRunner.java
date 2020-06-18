1ce8662e3553b2c83923ef7d87043d108aa753b4
jinwoo park <swat018@gmail.com>
2020-06-18 23:07:09 +0900

    JPA 프로그래밍 - 프로젝트 세팅

---
+++ src/main/java/com/swat018/springdatademo/JpaRunner.java
@@ -0,0 +1,29 @@
+package com.swat018.springdatademo;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("jinwoo");
+        account.setPassword("1234");
+
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+    }
+}
