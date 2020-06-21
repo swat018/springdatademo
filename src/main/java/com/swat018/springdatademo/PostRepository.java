b48c3d95e27ebe2d8fe99217b266d75789d79c5f
jinwoo park <swat018@gmail.com>
2020-06-21 23:57:12 +0900

    스프링 데이터 Common: Repository

--- src/main/java/com/swat018/springdatademo/PostRepository.java
+++ src/main/java/com/swat018/springdatademo/PostRepository.java
@@ -1,7 +1,11 @@
 package com.swat018.springdatademo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContains(String title, Pageable pagealbve);
 
+    long countByTitleContains(String title);
 }
